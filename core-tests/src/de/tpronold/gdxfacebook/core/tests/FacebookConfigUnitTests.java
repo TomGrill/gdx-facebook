24fa527f068c13520e48a368f521f6fb9bdd5129
Thomas Pronold <tp@tpronold.de>
2015-03-25 22:14:39 +0100

    FacebookConfig unit tests

---
+++ core-tests/src/de/tpronold/gdxfacebook/core/tests/FacebookConfigUnitTests.java
@@ -0,0 +1,26 @@
+package de.tpronold.gdxfacebook.core.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.tpronold.gdxfacebook.core.FacebookConfig;
+
+public class FacebookConfigUnitTests {
+
+	FacebookConfig fixture;
+
+	@Before
+	public void setup() {
+		fixture = new FacebookConfig();
+	}
+
+	@Test
+	public void defaultValues() {
+		assertEquals(false, fixture.ENABLE_ANDROID);
+		assertEquals(false, fixture.ENABLE_DESKTOP);
+		assertEquals(false, fixture.ENABLE_HTML);
+		assertEquals(false, fixture.ENABLE_IOS);
+	}
+}
