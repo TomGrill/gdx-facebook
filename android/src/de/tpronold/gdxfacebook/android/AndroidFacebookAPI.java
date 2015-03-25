af19b4f3f5800dba321f51794235f77ad856cb12
Thomas Pronold <tp@tpronold.de>
2015-03-25 22:14:24 +0100

    AndroidFacebookAPI basic implementation added

---
+++ android/src/de/tpronold/gdxfacebook/android/AndroidFacebookAPI.java
@@ -0,0 +1,26 @@
+package de.tpronold.gdxfacebook.android;
+
+import de.tpronold.gdxfacebook.core.FacebookAPI;
+import de.tpronold.gdxfacebook.core.ResponseListener;
+
+public class AndroidFacebookAPI implements FacebookAPI {
+
+	@Override
+	public void signin(ResponseListener responseListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isSignedin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void signout() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
