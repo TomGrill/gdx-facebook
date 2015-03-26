ae96db5730720975d9500010688059bd38bd8c18
Thomas Pronold <tp@tpronold.de>
2015-03-26 15:55:49 +0100

    refactoring

--- core-tests/src/de/tpronold/gdxfacebook/core/tests/FacebookGraphRequestUnitTests.java
+++ core-tests/src/de/tpronold/gdxfacebook/core/tests/FacebookGraphRequestUnitTests.java
@@ -29,21 +29,18 @@ public class FacebookGraphRequestUnitTests {
 
 	private FacebookGraphRequest fixture;
 
-	private static final String invalidTokenError = "{\"error\":{\"message\":\"Invalid OAuth access token.\",\"type\":\"OAuthException\",\"code\":190}}";
+	private static final String invalidTokenErrorMeRequest = "{\"error\":{\"message\":\"Invalid OAuth access token.\",\"type\":\"OAuthException\",\"code\":190}}";
+	private static final String validMeRequest = "{\"id\":\"444444444444\",\"email\":\"tom\u0040test.de\",\"first_name\":\"Tom\",\"gender\":\"male\",\"last_name\":\"Grill\",\"link\":\"https://www.facebook.com/app_scoped_user_id/443244444/\",\"locale\":\"de_DE\",\"name\":\"Tom Grill\",\"timezone\":1,\"updated_time\":\"2012-07-03T18:55:17+0000\",\"verified\":true}";
 
 	private String resultForVerifyTokenFailsWhenWrong;
 	private String resultForVerifyTokenFailsWhenWrongAsBackgroundThread;
 
 	@Before
 	public void setup() {
-
-		// HeadlessApplicationConfiguration conf = new
-		// HeadlessApplicationConfiguration();
-		// new HeadlessApplication(new ApplicationAdapter() {
-		// }, conf);
-
 		Gdx.net = mock(Net.class);
+	}
 
+	private void makeSendHttpRequestFail() {
 		Mockito.doAnswer(new Answer<Object>() {
 			public Object answer(InvocationOnMock invocation) {
 
@@ -60,7 +57,7 @@ public class FacebookGraphRequestUnitTests {
 
 					@Override
 					public String getResultAsString() {
-						return invalidTokenError;
+						return invalidTokenErrorMeRequest;
 					}
 
 					@Override
@@ -87,11 +84,57 @@ public class FacebookGraphRequestUnitTests {
 				return null;
 			}
 		}).when(Gdx.net).sendHttpRequest(any(HttpRequest.class), any(HttpResponseListener.class));
+	}
 
+	private void makeSendHttpRequestSucceed() {
+		Mockito.doAnswer(new Answer<Object>() {
+			public Object answer(InvocationOnMock invocation) {
+
+				Object[] args = invocation.getArguments();
+
+				HttpResponseListener listener = (HttpResponseListener) args[1];
+
+				listener.handleHttpResponse(new HttpResponse() {
+
+					@Override
+					public HttpStatus getStatus() {
+						return null;
+					}
+
+					@Override
+					public String getResultAsString() {
+						return validMeRequest;
+					}
+
+					@Override
+					public InputStream getResultAsStream() {
+						return null;
+					}
+
+					@Override
+					public byte[] getResult() {
+						return null;
+					}
+
+					@Override
+					public Map<String, List<String>> getHeaders() {
+						return null;
+					}
+
+					@Override
+					public String getHeader(String name) {
+						return null;
+					}
+				});
+
+				return null;
+			}
+		}).when(Gdx.net).sendHttpRequest(any(HttpRequest.class), any(HttpResponseListener.class));
 	}
 
 	@Test
 	public void verifyTokenFailsWhenWrong() {
+		makeSendHttpRequestFail();
 		fixture = new FacebookGraphRequest(FacebookGraphRequestType.ME, "", new JSONRequestListener() {
 			@Override
 			public void requestResult(String jsonBody) {
@@ -106,11 +149,12 @@ public class FacebookGraphRequestUnitTests {
 			} catch (InterruptedException e) {
 			}
 		}
-		assertEquals(invalidTokenError, resultForVerifyTokenFailsWhenWrong);
+		assertEquals(invalidTokenErrorMeRequest, resultForVerifyTokenFailsWhenWrong);
 	}
 
 	@Test
 	public void verifyTokenFailsWhenWrongAsBackgroundThread() {
+		makeSendHttpRequestFail();
 		fixture = new FacebookGraphRequest(FacebookGraphRequestType.ME, "", new JSONRequestListener() {
 			@Override
 			public void requestResult(String jsonBody) {
@@ -125,7 +169,7 @@ public class FacebookGraphRequestUnitTests {
 			} catch (InterruptedException e) {
 			}
 		}
-		assertEquals(invalidTokenError, resultForVerifyTokenFailsWhenWrongAsBackgroundThread);
+		assertEquals(invalidTokenErrorMeRequest, resultForVerifyTokenFailsWhenWrongAsBackgroundThread);
 	}
 
 }
