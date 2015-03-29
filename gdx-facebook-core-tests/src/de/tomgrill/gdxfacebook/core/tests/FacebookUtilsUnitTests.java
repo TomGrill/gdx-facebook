be98b899f330b06b9f508cf4a514c4a5722a9fb6
Thomas Pronold <tp@tpronold.de>
2015-03-29 19:48:01 +0200

    added permissionSplitToList method

--- gdx-facebook-core-tests/src/de/tomgrill/gdxfacebook/core/tests/FacebookUtilsUnitTests.java
+++ gdx-facebook-core-tests/src/de/tomgrill/gdxfacebook/core/tests/FacebookUtilsUnitTests.java
@@ -17,4 +17,14 @@ public class FacebookUtilsUnitTests {
 		assertEquals("s3", FacebookUtils.permissionSplit(s)[2]);
 		assertEquals("s4", FacebookUtils.permissionSplit(s)[3]);
 	}
+
+	@Test
+	public void permissionSplitToList() {
+		String s = "s1 ,  s2,s3,  s4   ";
+
+		assertEquals("s1", FacebookUtils.permissionSplitToList(s).get(0));
+		assertEquals("s2", FacebookUtils.permissionSplitToList(s).get(1));
+		assertEquals("s3", FacebookUtils.permissionSplitToList(s).get(2));
+		assertEquals("s4", FacebookUtils.permissionSplitToList(s).get(3));
+	}
 }
--- gdx-facebook-core/src/de/tomgrill/gdxfacebook/core/FacebookUtils.java
+++ gdx-facebook-core/src/de/tomgrill/gdxfacebook/core/FacebookUtils.java
@@ -1,11 +1,28 @@
 package de.tomgrill.gdxfacebook.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FacebookUtils {
 	public static String[] permissionSplit(String stringToSplit) {
 		String[] result = stringToSplit.split(",");
 
 		for (int i = 0; i < result.length; i++) {
 			result[i] = result[i].trim();
+		}
+
+		return result;
+
+	}
+
+	public static List<String> permissionSplitToList(String stringToSplit) {
+
+		String[] tmpResult = permissionSplit(stringToSplit);
+
+		List<String> result = new ArrayList<String>();
+
+		for (int i = 0; i < tmpResult.length; i++) {
+			result.add(tmpResult[i]);
 		}
 
 		return result;
