b20d56ebbb74ee9f93af004930ba6262ed50ec9a
Thomas Pronold <tp@tpronold.de>
2015-10-09 12:31:54 +0200

    added GDXFacebookConfig

---
+++ gdx-facebook-core/src/de/tomgrill/gdxfacebook/core/GDXFacebookConfig.java
@@ -0,0 +1,14 @@
+package de.tomgrill.gdxfacebook.core;
+
+public class GDXFacebookConfig {
+
+    /**
+     * gdx-facebook stores certain values and Facebook tokens in this file. (It's not secured)
+     */
+    public String PREF_FILENAME = ".gdxFacebookTokenData";
+
+    /**
+     * Put you Facebook App ID here. Get it from: https://developers.facebook.com/
+     */
+    public String APP_ID = "";
+}
