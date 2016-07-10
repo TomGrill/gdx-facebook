7b93ddfba0a90feebd8dc0e251eba9a90dbf9e42
Thomas Pronold <tp@tpronold.de>
2016-07-10 19:21:38 +0200

    bumped version to 1.2.2

--- core/src/de/tomgrill/gdxfacebook/core/GDXFacebookVars.java
+++ core/src/de/tomgrill/gdxfacebook/core/GDXFacebookVars.java
@@ -18,6 +18,6 @@
 package de.tomgrill.gdxfacebook.core;
 
 public class GDXFacebookVars {
-	public static final String VERSION = "1.2.2-SNAPSHOT";
+	public static final String VERSION = "1.2.2";
 	public static final String LOG_TAG = "gdx-facebook (" + VERSION + ")";
 }
